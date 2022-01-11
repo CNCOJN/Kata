3d0741baa456ee4ea44d50d88528413a56dce270
REN CHEN <cncojn.ren@outlook.com>
2022-01-11 13:55:08 +0800

    finish StringAverage project and unit tests

---
+++ Kata/src/StringAverage/Case.cs
@@ -0,0 +1,25 @@
+﻿namespace StringAverage;
+
+public class Case
+{
+    private readonly string[] _numbersInLetters =
+        new[] {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    
+    private const string NotAvailable = "n/a";
+
+    public string StringAverage1(string? source)
+    {
+        if (string.IsNullOrEmpty(source?.Trim())) return NotAvailable;
+        var numbersInDigits = ConertNumberInLettersToDigits(source);
+        return AreNumbersInValid(numbersInDigits) ? NotAvailable : GetAverageInNumericLetters(numbersInDigits);
+    }
+
+    private IEnumerable<int> ConertNumberInLettersToDigits(string input) => input
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(x => Array.IndexOf(_numbersInLetters, x.ToLowerInvariant()));
+
+    private bool AreNumbersInValid(IEnumerable<int> numbers) => numbers.Any(x => x == -1);
+
+    private string GetAverageInNumericLetters(IEnumerable<int> numbersInDigits) =>
+        _numbersInLetters[(int) numbersInDigits.Average()];
+}
---
+++ Kata/tests/StringAverage.Tests/CaseTests.cs
@@ -0,0 +1,98 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace StringAverage.Tests
+{
+    [TestFixture]
+    public class CaseTests
+    {
+        private Case? _case;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _case = new();
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void StringAverage1_GivenNullOrEmptyString_ReutrnsNA(string input)
+        {
+            // Arrange
+            string message = "n/a";
+            // Act
+            var result = _case?.StringAverage1(input);
+            // Assert
+            result.Should().Be(message);
+        }
+
+        [TestCase(" ")]
+        [TestCase("  ")]
+        [TestCase("    ")]
+        public void StringAverage1_GivenBlankSpaces_ReutrnsNA(string input)
+        {
+            // Arrange
+            string message = "n/a";
+            // Act
+            var result = _case?.StringAverage1(input);
+            // Assert
+            result.Should().Be(message);
+        }
+
+        [TestCase("one two three")]
+        [TestCase("One tWo threE")]
+        [TestCase("three two one")]
+        [TestCase("Three tWo onE")]
+        [TestCase("ONE TWO THREE")]
+        public void StringAverage1_GivenOneTwoThree_ReturnsTwo(string input)
+        {
+            // Arrange
+            string message = "two";
+            // Act
+            var result = _case?.StringAverage1(input);
+            // Assert
+            result.Should().Be(message);
+        }
+
+        [TestCase("one  two three")]
+        [TestCase("One  tWo  threE")]
+        [TestCase("three  two one")]
+        [TestCase("Three tWo  onE")]
+        [TestCase("ONE  TWO  THREE")]
+        public void StringAverage1_GivenOneTwoThreeAndAddSpacesBetweenThem_ReturnsTwo(string input)
+        {
+            // Arrange
+            string message = "two";
+            // Act
+            var result = _case?.StringAverage1(input);
+            // Assert
+            result.Should().Be(message);
+        }
+
+        [TestCase("one two twelve")]
+        [TestCase("twelve two one")]
+        public void StringAverage1_GivenGreaterThanNine_ReturnsNA(string input)
+        {
+            // Arrange
+            string message = "n/a";
+            // Act
+            var result = _case?.StringAverage1(input);
+            // Assert
+            result.Should().Be(message);
+        }
+
+        [TestCase("one two Not_A_Number")]
+        [TestCase("one  two  Not_A_Number")]
+        [TestCase("Not_A_Number two one")]
+        [TestCase("Not_A_Number  two  one")]
+        public void StringAverage1_GivenStringContainsNonNumber_ReturnsNA(string input)
+        {
+            // Arrange
+            string message = "n/a";
+            // Act
+            var result = _case?.StringAverage1(input);
+            // Assert
+            result.Should().Be(message);
+        }
+    }
+}
